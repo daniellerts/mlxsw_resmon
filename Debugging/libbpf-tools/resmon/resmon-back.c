pmachata/mlxsw

resmon: Introduce HW backend
The HW backend will expect that resmon is running on a Spectrum-type
machine and will install BPF to capture interesting EMADs, and probe
devlink to discover resource pool size.

In this patch, introduce a new module, devlink-dl, which handles the
devlink interface. Further introduce the HW backend, and implement the
get_capacity callback that dispatches to the devlink-dl module.

The new backend is not accessible at this point yet.

Signed-off-by: Danielle Ratson <danieller@nvidia.com>
Co-developed-by: Petr Machata <petrm@nvidia.com>
Signed-off-by: Petr Machata <petrm@nvidia.com>

Debugging/libbpf-tools/src/resmon/resmon-back.c
@@ -5,6 +5,46 @@
 
 #include "resmon.h"
 
+struct resmon_back_hw {
+	struct resmon_back base;
+};
+
+static struct resmon_back *resmon_back_hw_init(void)
+{
+	struct resmon_back_hw *back;
+
+	back = malloc(sizeof(*back));
+	if (back == NULL)
+		return NULL;
+
+	*back = (struct resmon_back_hw) {
+		.base.cls = &resmon_back_cls_hw,
+	};
+
+	return &back->base;
+}
+
+static void resmon_back_hw_fini(struct resmon_back *base)
+{
+	struct resmon_back_hw *back =
+		container_of(base, struct resmon_back_hw, base);
+
+	free(back);
+}
+
+static int resmon_back_hw_get_capacity(struct resmon_back *back,
+				       uint64_t *capacity,
+				       char **error)
+{
+	return resmon_dl_get_kvd_size(capacity, error);
+}
+
+const struct resmon_back_cls resmon_back_cls_hw = {
+	.init = resmon_back_hw_init,
+	.fini = resmon_back_hw_fini,
+	.get_capacity = resmon_back_hw_get_capacity,
+};
+
 struct resmon_back_mock {
 	struct resmon_back base;
 };

daniellerts/mlxsw_resmon

Debugging/libbpf-tools/resmon/resmon-back.c
@@ -1,8 +1,66 @@
 // SPDX-License-Identifier: BSD-3-Clause OR GPL-2.0
 #include <errno.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 #include "resmon.h"
+
+struct resmon_back_hw {
+	struct resmon_back base;
+	struct resmon_dl *dl;
+};
+
+static struct resmon_back *resmon_back_hw_init(void)
+{
+	struct resmon_back_hw *back;
+	struct resmon_dl *dl;
+
+	back = malloc(sizeof(*back));
+	if (back == NULL)
+		return NULL;
+
+	dl = resmon_dl_create();
+	if (dl == NULL) {
+		fprintf(stderr, "Failed to open netlink socket\n");
+		goto free_back;
+	}
+
+	*back = (struct resmon_back_hw) {
+		.base.cls = &resmon_back_cls_hw,
+		.dl = dl,
+	};
+
+	return &back->base;
+
+free_back:
+	free(back);
+	return NULL;
+}
+
+static void resmon_back_hw_fini(struct resmon_back *base)
+{
+	struct resmon_back_hw *back =
+		container_of(base, struct resmon_back_hw, base);
+
+	resmon_dl_destroy(back->dl);
+	free(back);
+}
+
+static int resmon_back_hw_get_capacity(struct resmon_back *base,
+				       uint64_t *capacity,
+				       char **error)
+{
+	struct resmon_back_hw *back =
+		container_of(base, struct resmon_back_hw, base);
+
+	return resmon_dl_get_kvd_size(back->dl, capacity, error);
+}
+
+const struct resmon_back_cls resmon_back_cls_hw = {
+	.init = resmon_back_hw_init,
+	.fini = resmon_back_hw_fini,
+	.get_capacity = resmon_back_hw_get_capacity,
+};
 
 struct resmon_back_mock {
 	struct resmon_back base;